68b31d1a22d07fa5505f4eb1fb9d88f724c9bfd1
wanazairi <wanazairi@gmail.com>
2018-05-16 19:31:08 +0800

    wana

--- ajax_example.php
+++ ajax_example.php
@@ -80,7 +80,7 @@
     
     <body>
 
-        <h3>rrrrrrrmmfsas</h3>
+        <h3>Read data from database</h3>
         <ul id="names"></ul>
 
         <h3 id="updated">Update passwords</h3>
@@ -88,7 +88,7 @@
         <button id="btn1">Update</button>
 
         <h3 id="inserted">Insert user</h3>
-        <p>Username la: </p><input type="text" id="userName">
+        <p>Username: </p><input type="text" id="userName">
         <p>Password: </p><input type="text" id="password">
         <p>Email: </p><input type="text" id="email">
         <p>Address: </p><input type="text" id="address">
