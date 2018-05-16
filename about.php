1835b3217097d941ffd8c1eb95c42ec3da54e0e7
wanazairi <wanazairi@gmail.com>
2018-05-16 20:25:23 +0800

    wana

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | About :: w3layouts</title>
+<title>Mint :: About </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Staple Food Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
