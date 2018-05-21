8a9021badcfc9ae7eeb47f5b7f1abba70eea0a5d
syazanihussin <syazanihussin@gmail.com>
2018-05-21 23:30:46 +0800

    syazani

--- ondelivery.php
+++ ondelivery.php
@@ -207,9 +207,36 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<script>
 						var map;
 						function initMap() {
-							map = new google.maps.Map(document.getElementById('map'), {
-							center: {lat: -34.397, lng: 150.644},
-							zoom: 8
+							var directionsService = new google.maps.DirectionsService;
+							var directionsDisplay = new google.maps.DirectionsRenderer;
+							var map = new google.maps.Map(document.getElementById('map'), {
+								zoom: 6,
+								center: {lat: 41.85, lng: -87.65}
+							});
+							directionsDisplay.setMap(map);
+							calculateAndDisplayRoute(directionsService, directionsDisplay);
+						}
+
+						function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+							var waypts = []
+							waypts.push({
+								location: '<?php echo $supplier[0]['address']; ?>',
+								stopover: true
+							});
+
+							directionsService.route({
+								origin: 'UPM Kolej 6, Serdang, Selangor, Malaysia',
+								destination: '<?php echo $orderss[0]['deliveryTo']; ?>',
+								waypoints: waypts,
+								optimizeWaypoints: true,
+								travelMode: 'DRIVING'
+
+							}, function(response, status) {
+								if (status === 'OK') {
+									directionsDisplay.setDirections(response);
+								} else {
+									window.alert('Directions request failed due to ' + status);
+								}
 							});
 						}
 					</script>
