03bfbea5002aa159fc6c695004aa1f5d47d4af12
fatihah81 <fatihahblack@gmail.com>
2018-05-24 16:41:58 +0800

    teah

--- faq.php
+++ faq.php
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | FAQ :: w3layouts</title>
+<title>Mint :: About </title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Staple Food Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
@@ -34,22 +34,39 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="w3ls-header"><!-- header-one --> 
 				<div class="container">
 					<div class="w3ls-header-left">
-						<p>Free home delivery at your doorstep For Above $30</p>
+						<p>Food delivery platform | UPM</p>
 					</div>
 					<div class="w3ls-header-right">
-						<ul> 
-							<li class="head-dpdn">
-								<i class="fa fa-phone" aria-hidden="true"></i> Call us: +01 222 33345 
-							</li> 
-							<li class="head-dpdn">
-								<a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
-							</li> 
-							<li class="head-dpdn">
-								<a href="signup.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Signup</a>
-							</li> 
-							<li class="head-dpdn">
-								<a href="offers.php"><i class="fa fa-gift" aria-hidden="true"></i> Offers</a>
-							</li> 
+						<ul>
+							<?php
+								if(!isset($_SESSION['customer'])){
+									echo '
+									<li class="head-dpdn">
+										<a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+									</li> 
+									<li class="head-dpdn">
+										<a href="signup.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Signup</a>
+									</li> <li class="head-dpdn">
+										<a href="register.php"><i class="fa fa-car" aria-hidden="true"></i> Join our delivery team</a>
+									</li>
+									';
+								}
+
+								else if(isset($_SESSION['customer'])  && count($_SESSION['customer']) == 0){
+									echo '
+									<li class="head-dpdn">
+										<a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+									</li> 
+									<li class="head-dpdn">
+										<a href="signup.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Signup</a>
+									</li> <li class="head-dpdn">
+										<a href="register.php"><i class="fa fa-car" aria-hidden="true"></i> Join our delivery team</a>
+									</li>
+									';
+								}
+							?>
+							 
+							
 							<li class="head-dpdn">
 								<a href="help.php"><i class="fa fa-question-circle" aria-hidden="true"></i> Help</a>
 							</li>
@@ -71,63 +88,40 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>  
-							<h1><a href="index.php">Staple<span>Best Food Collection</span></a></h1>
+							<h1><a href="index.php">Mint<span>An Oasis Of Food</span></a></h1>
 						</div> 
 						<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 							<ul class="nav navbar-nav navbar-right">
 								<li><a href="index.php">Home</a></li>	
-								<!-- Mega Menu -->
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu <b class="caret"></b></a>
-									<ul class="dropdown-menu multi-column columns-3">
-										<div class="row">
-											<div class="col-sm-4">
-												<ul class="multi-column-dropdown">
-													<h6>Food type</h6>  
-													<li><a href="restaurant.php">Breakfast</a></li> 
-													<li><a href="restaurant.php">Lunch</a></li> 
-													<li><a href="restaurant.php">Dinner</a></li> 
-												</ul>
-											</div>
-											<div class="col-sm-4">
-												<ul class="multi-column-dropdown">
-													<h6>Cuisine</h6> 
-													<li><a href="restaurant.php">Indian Recipes</a></li> 
-													<li><a href="restaurant.php">American Recipes</a></li> 
-													<li><a href="restaurant.php">French Recipes</a></li> 
-													<li><a href="restaurant.php">Italian Recipes</a></li> 
-												</ul>
-											</div>
-											<div class="col-sm-4">
-												<ul class="multi-column-dropdown">
-													<h6>Box type</h6> 
-													<li><a href="restaurant.php">Diet</a></li> 
-													<li><a href="restaurant.php">Mini</a></li> 
-													<li><a href="restaurant.php">Regular</a></li> 
-													<li><a href="restaurant.php">Special</a></li> 
-												</ul>
-											</div> 
-											<div class="clearfix"></div>
-										</div>
-									</ul>
-								</li>
-								<li><a href="about.php">About</a></li> 
-								<li class="w3pages"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a href="icons.php">Web Icons</a></li>
-										<li><a href="codes.php">Short Codes</a></li>     
-									</ul>
-								</li>  
+								<li><a href="about.php" class="active">About</a></li> 
+								<li><a href="restaurant.php" >Restaurant</a></li>
+								<li><a href="products.php" >Food</a></li>
 								<li><a href="contact.php">Contact Us</a></li>
+								<?php
+								if(isset($_SESSION['customer'])  && count($_SESSION['customer']) != 0){
+									echo '
+									<li class="w3pages"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $_SESSION['customer'][0]['username'] . ' <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="logout.php">Logout</a></li>    
+										</ul>
+									</li>';
+								}
+							?>
 							</ul>
 						</div>
-						<div class="cart cart box_1"> 
-							<form action="#" method="post" class="last"> 
-								<input type="hidden" name="cmd" value="_cart" />
-								<input type="hidden" name="display" value="1" />
-								<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
-							</form>   
-						</div> 
+						<?php
+							if(isset($_SESSION['customer'])  && count($_SESSION['customer']) != 0){
+								echo '
+								<div class="cart cart box_1"> 
+									<form action="#" method="post" class="last"> 
+										<input type="hidden" name="cmd" value="_cart" />
+										<input type="hidden" name="display" value="1" />
+										<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
+									</form>   
+								</div> 
+								';
+							}
+						?>
 					</nav>
 				</div>
 			</div>
@@ -155,42 +149,56 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="container"> 
 			<h3 class="w3ls-title w3ls-title1">Frequently asked questions(FAQ)</h3> 
 			<ul class="faq">
-				<li class="item1"><a href="#" title="click here">How much salary is offered to work?</a>
+				<li class="item1"><a href="#" title="click here">How does it work?</a>
 					<ul>
-						<li class="subitem1"><p>Can delivery up to what time?</p></li>										
+						<li class="subitem1"><p>Enter your address to find some of your favorite local restaurants available for delivery. 
+						Select items you'd like to have delivered and enter any special instructions (if applicable). Pay with cash, or credit, 
+						then kick back and relax; we've got it from here. Your friendly local food runner will be knocking on your door before you know it! </p></li>										
 					</ul>
 				</li>
-				<li class="item2"><a href="#" title="click here">Can delivery up to what time?</a>
+				<li class="item2"><a href="#" title="click here">Did my order go through?</a>
 					<ul>
-						<li class="subitem1"><p> Tincidunt ut laoreet dolore At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>If you recieved an email confirmation you're all set! If not, just chat in or give us
+						 a quick call and we can help.</p></li>										
 					</ul>
 				</li>
-				<li class="item3"><a href="#" title="click here">Just UPM student can work here?</a>
+				<li class="item3"><a href="#" title="click here">Can I change/cancel my order after it's placed?</a>
 					<ul>
-						<li class="subitem1"><p>Dincidunt ut laoreet dolore At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>Sometimes. If we can reach the restaurant before they have started making it, we might 
+						be able to adjust the order. Sooner we know, the better the chances.</p></li>										
 					</ul>
 				</li>
-				<li class="item4"><a href="#" title="click here">Is the food here all covered halal?</a>
+				<li class="item4"><a href="#" title="click here">How much does it cost to use the service?</a>
 					<ul>
-						<li class="subitem1"><p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>Price that Mint provide is able to belong. Typical to student UPM. </p></li>										
 					</ul>
 				</li> 
-				<li class="item5"><a href="#" title="click here">How can I register to be a delivery guy in Mint ?</a>
+				<li class="item5"><a href="#" title="click here">How long will it take to get the order?</a>
 					<ul>
-						<li class="subitem1"><p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>All orders are delivered in typically less than an hour. We try to get your food to you as fast as possible. 
+						However, if the order will take more than one hour, the restaurant will let you know about it.</p></li>										
 					</ul>
 				</li>
-				<li class="item6"><a href="#" title="click here">Does girl can be the deliver ?</a>
+				<li class="item6"><a href="#" title="click here">I got my order and some items are missing. What should I do now?</a>
 					<ul>
-						<li class="subitem1"><p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>If there is a discrepancy in the order, please contact us and we’ll set things right.</p></li>										
 					</ul>
 				</li>
-				<li class="item7"><a href="#" title="click here">Can we claim our gas transport ?</a>
+				<li class="item7"><a href="#" title="click here">Is there a Mint app?</a>
 					<ul>
-						<li class="subitem1"><p>At vero eos et Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod  consectetuer adipiscing elit, sed diam nonummy nibh euismod accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p></li>										
+						<li class="subitem1"><p>We only using website that people can search www.mint.com.my</p></li>										
 					</ul>
 				</li>
-				 
+				<li class="item7"><a href="#" title="click here">Can UPM student join Mint delivery team?</a>
+					<ul>
+						<li class="subitem1"><p>Yes, Everyone can join our team and earn money with our delivery team</p></li>										
+					</ul>
+				</li>
+				<li class="item7"><a href="#" title="click here">The price is included GST?</a>
+					<ul>
+						<li class="subitem1"><p>Not GST included in our delivery. Charge based on food and delivery.</p></li>										
+					</ul>
+				</li>
 			</ul> 
 			<!-- script for tabs -->
 			<script type="text/javascript">
@@ -231,12 +239,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<li><a href="#" class="fa fa-dribbble icon dribbble"> </a></li>
 					<li><a href="#" class="fa fa-rss icon rss"> </a></li> 
 				</ul> 
-				<ul class="apps"> 
-					<li><h4>Download Our app : </h4> </li>
-					<li><a href="#" class="fa fa-apple"></a></li>
-					<li><a href="#" class="fa fa-windows"></a></li>
-					<li><a href="#" class="fa fa-android"></a></li>
-				</ul> 
 			</div> 
 			<div class="col-md-6 subscribe-right">
 				<h3 class="w3ls-title">Subscribe to Our <br><span>Newsletter</span></h3>  
@@ -260,8 +262,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<ul>
 						<li><a href="about.php">About Us</a></li>
 						<li><a href="contact.php">Contact Us</a></li>  
-						<li><a href="careers.php">Careers</a></li>  
-						<li><a href="help.php">Partner With Us</a></li>   
+						<li><a href="careers.php">Careers</a></li>   
 					</ul>
 				</div> 
 				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
@@ -296,7 +297,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	</div>
 	<div class="copyw3-agile"> 
 		<div class="container">
-			<p>&copy; 2017 Staple Food. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			<p>&copy; 2017 Staple Food. All rights reserved | Design by Mint</a></p>
 		</div>
 	</div>
 	<!-- //footer -->
