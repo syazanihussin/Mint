ef987af42646eedc2d0ae1f724adefb5721bb78b
aimanzairi <aimanzairi23@gmail.com>
2018-05-25 12:15:23 +0800

    a

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 								if(isset($_SESSION['customer']) && count($_SESSION['customer']) != 0){
 									echo '
 									<li class="w3pages"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . 
-									$_SESSION['customer'][0]['$username'] . ' <span class="caret"></span></a>
+									$_SESSION['customer'][0]['username'] . ' <span class="caret"></span></a>
 										<ul class="dropdown-menu">
 											<li><a href="logout.php">Logout</a></li>    
 										</ul>
